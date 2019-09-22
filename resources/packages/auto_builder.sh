5f5220730bbb65c5c93d99be185f40f27bbe66b0
oxedions <oxedions@gmail.com>
2019-09-22 17:19:45 +0200

    Adding equipments level alerts definition in prometheus role

--- resources/packages/auto_builder.sh
+++ resources/packages/auto_builder.sh
@@ -1,20 +1,49 @@
+echo
+echo "  __                 "
+echo " '. \                "
+echo "  '- \               "
+echo "   / /_         .---."
+echo "  / | \\,.\/--.//    )"
+echo "  |  \//        )/  / "
+echo "   \  ' ^ ^    /    )____.----..  6"
+echo "    '.____.    .___/            \._) "
+echo "       .\/.                      )"
+echo "        '\                       /"
+echo "        _/ \/    ).        )    ("
+echo "       /#  .!    |        /\    /"
+echo "       \  C// #  /'-----''/ #  / "
+echo "    .   'C/ |    |    |   |    |mrf  ,"
+echo "    \), .. .'OOO-'. ..'OOO'OOO-'. ..\(,"
+echo
+echo "  BlueBanquise packages builder"
+echo
+echo " Detecting distribution..."
+distribution=$(grep ^NAME /etc/os-release | awk -F '"' '{print $2}')
+distribution_version=$(grep ^VERSION_ID /etc/os-release | awk -F '"' '{print $2}')
+echo " Found $distribution $distribution_version"
+working_directory=/dev/shm
+echo " Working directory: $working_directory"
+
 set -x
 echo "Cleaning"
-rm -Rf build
+rm -Rf $working_directory/build
+mkdir $working_directory/build
+mkdir $working_directory/sources
 
+echo " Installing needed packages... may take sime time."
 yum install rpm-build genisoimage xz xz-devel automake autoconf python36 bzip2-devel openssl-devel zlib-devel readline-devel pam-devel perl-ExtUtils-MakeMaker grub2-tools-extra grub2-efi-x64-modules gcc -y
 
-mkdir build
-cd build
-
 # iPXE
-#tar cvzf bluebanquise_ipxe-1.0.1.tar.gz ../bluebanquise_ipxe-1.0.1
-#rpmbuild -ta bluebanquise_ipxe-1.0.1.tar.gz
+tar cvzf $working_directory/sources/bluebanquise_ipxe-1.0.2.tar.gz bluebanquise_ipxe-1.0.2
+rpmbuild -ta --buildroot $working_directory/build $working_directory/sources/bluebanquise_ipxe-1.0.2.tar.gz
 
 # Atftp
-#wget https://freefr.dl.sourceforge.net/project/atftp/atftp-0.7.2.tar.gz
-#tar xvzf atftp-0.7.2.tar.gz
-#/usr/bin/cp -f ../atftp-0.7.2/* atftp-0.7.2/
+if [ ! -f $working_directory/sources/atftp-0.7.2.tar.gz ]; then
+  wget -P $working_directory/sources/ https://freefr.dl.sourceforge.net/project/atftp/atftp-0.7.2.tar.gz 
+fi
+cd $working_directory/sources/
+tar xvzf atftp-0.7.2.tar.gz
+/usr/bin/cp -f ../atftp-0.7.2/* atftp-0.7.2/
 #rm -f atftp-0.7.2/redhat/atftp.spec
 #tar cvzf atftp-0.7.2.tar.gz atftp-0.7.2
 #rpmbuild -ta atftp-0.7.2.tar.gz
