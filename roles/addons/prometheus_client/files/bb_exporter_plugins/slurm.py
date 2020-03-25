b5769370c495c2b7755fb353a45ba9aeb7a96e52
oxedions <oxedions@gmail.com>
2020-03-25 16:00:24 +0100

    Fix: update github URL

--- roles/addons/prometheus_client/files/bb_exporter_plugins/slurm.py
+++ roles/addons/prometheus_client/files/bb_exporter_plugins/slurm.py
@@ -1,7 +1,7 @@
 # Slurm plugin for bb exporter
 # To export sinfo metrics, not slurmd or slurmctld daemons. If you need to watch these, use services plugin.
 # 2020 - Benoît Leveugle <benoit.leveugle@sphenisc.com>
-# https://github.com/oxedions/bluebanquise - MIT license
+# https://github.com/bluebanquise/bluebanquise - MIT license
 
 import subprocess
 from prometheus_client.core import GaugeMetricFamily
