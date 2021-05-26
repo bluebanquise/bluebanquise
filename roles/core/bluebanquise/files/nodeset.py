620a631d591d540710fcf634129370c5b6e8d1b5
oxedions <oxedions@gmail.com>
2021-05-26 22:15:28 +0200

    Feat: plugin - Add nodeset filter

---
+++ roles/core/bluebanquise/files/nodeset.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Make coding more python3-ish
+# From Bruno Travouillon, @btravouillon
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.errors import AnsibleFilterError
+from ClusterShell.NodeSet import NodeSet
+
+def nodeset(nodes_list):
+    '''Convert a list of nodes to ClusterShell's NodeSet'''
+    nodeset = NodeSet(",".join(nodes_list))
+    return nodeset
+
+class FilterModule(object):
+    ''' NodeSet Jinja2 filter. '''
+
+    def filters(self):
+        return {
+            'nodeset': nodeset,
+        }
