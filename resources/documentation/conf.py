8aa5faeed3f42da31dccc7658a0581404c453e6f
oxedions <oxedions@gmail.com>
2022-03-07 20:37:39 +0100

    up

--- resources/documentation/conf.py
+++ resources/documentation/conf.py
@@ -84,7 +84,7 @@ pygments_style = None
 #     }
 html_logo = "logo.png"
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
+html_theme_path = ["_themes"]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
