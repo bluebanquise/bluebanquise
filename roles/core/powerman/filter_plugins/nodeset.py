251a508c6f68eab3d406f4c5e59d95a74b6b99fc
oxedions <oxedions@gmail.com>
2022-04-07 10:57:35 +0200

    Fix: powerman - Filter update

--- roles/core/powerman/filter_plugins/nodeset.py
+++ roles/core/powerman/filter_plugins/nodeset.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 #
 # Make coding more python3-ish
+# Original idea from Bruno Travouillon, @btravouillon
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 from ansible.errors import AnsibleFilterError
 from ClusterShell.NodeSet import NodeSet
+from ansible.module_utils.common.text.converters import to_native
+
 
 def nodeset(nodes_list):
     '''Convert a list of nodes to ClusterShell's NodeSet'''
-    nodeset = NodeSet(",".join(nodes_list))
+
+    try:
+        nodeset = NodeSet(",".join(nodes_list))
+    except Exception as e:
+        raise AnsibleError('Error joining nodeset, original exception: %s' % to_native(e))
+
     return nodeset
+
 
 class FilterModule(object):
     ''' NodeSet Jinja2 filter. '''
