813307525aa73e2c56dbd854db6e5606f5d5e236
oxedions <oxedions@gmail.com>
2022-04-12 16:03:35 +0200

    Fix: typo and url

--- bootstrap_input.sh
+++ bootstrap_input.sh
@@ -4,9 +4,9 @@ INSTALL_GALAXY_REQUIREMENTS=true
 GATHER_PACKAGES=true
 GATHER_PACKAGES_UBUNTU_2004=true
 
-SAMPLE_INVENTORY=coconutshaker
+SAMPLE_INVENTORY=simple_cluster
 
-UBUNTU_2004_ISO_URL=http://192.168.122.1:8000/isos/ubuntu-20.04.4-live-server-amd64.iso
+UBUNTU_2004_ISO_URL=https://releases.ubuntu.com/20.04/ubuntu-20.04.4-live-server-amd64.iso
 UBUNTU_2004_ISO=ubuntu-20.04.4-live-server-amd64.iso
-REDHAT_8_ISO_URL=http://192.168.122.1:8000/isos/AlmaLinux-8.5-x86_64-dvd.iso
+REDHAT_8_ISO_URL=http://mirror.crexio.com/almalinux/8.5/isos/x86_64/AlmaLinux-8.5-x86_64-dvd.iso
 REDHAT_8_ISO=AlmaLinux-8.5-x86_64-dvd.iso
