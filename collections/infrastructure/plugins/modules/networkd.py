2618061e2db1ad7ea72a12d0877117a3283ba25a
oxedions <oxedions@gmail.com>
2022-11-01 10:51:05 -0400

    Dev - nic - flake8

--- collections/infrastructure/plugins/modules/networkd.py
+++ collections/infrastructure/plugins/modules/networkd.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
+# noqa: E501
 
-import re
 import os
 import pathlib
-import subprocess
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.common.text.converters import to_text
+
 
 class NetworkdModuleError(Exception):
     pass
+
 
 def same_list_file(list1, filepath):
     same = True
@@ -18,16 +18,17 @@ def same_list_file(list1, filepath):
         if len(list1) != len(filepath_lines):
             same = False
         else:
-            for i in range(0,len(list1),1):
+            for i in range(0, len(list1), 1):
                 if list1[i] != filepath_lines[i].replace("\n", ""):
                     same = False
     else:
         same = False
     return same
 
+
 def write_list_to_file(list1, filepath):
     path = pathlib.Path(filepath)
-    os.makedirs(path.parent, mode = 0o755, exist_ok=True)
+    os.makedirs(path.parent, mode=0o755, exist_ok=True)
     f = open(filepath, "w")
     for it in list1:
         f.write(it + "\n")
@@ -65,7 +66,6 @@ class Networkd(object):
         self.vlanid = module.params['vlanid']
         self.vlandev = module.params['vlandev']
         self.vlan_mapping = module.params['vlans']
-
 
     def generate_network(self):
         network = []
@@ -157,11 +157,13 @@ class Networkd(object):
 
         return netdev
 
+
 def main():
     # Parsing argument file
     module = AnsibleModule(
         argument_spec=dict(
-            state=dict(type='str', required=True, choices=['absent', 'present']),
+            state=dict(type='str', required=True,
+                       choices=['absent', 'present']),
             conn_name=dict(type='str', required=True),
             master=dict(type='str'),
             arp_interval=dict(type='str'),
@@ -180,9 +182,24 @@ def main():
             gw4=dict(type='str'),
             routes4=dict(type='list', elements='str'),
             dns4=dict(type='list', elements='str'),
-            method4=dict(type='str', choices=['auto', 'link-local', 'manual', 'shared', 'disabled']),
+            method4=dict(type='str',
+                         choices=[
+                          'auto',
+                          'link-local',
+                          'manual',
+                          'shared',
+                          'disabled'
+                         ]),
             mode=dict(type='str', default='balance-rr',
-                      choices=['802.3ad', 'active-backup', 'balance-alb', 'balance-rr', 'balance-tlb', 'balance-xor', 'broadcast']),
+                      choices=[
+                          '802.3ad',
+                          'active-backup',
+                          'balance-alb',
+                          'balance-rr',
+                          'balance-tlb',
+                          'balance-xor',
+                          'broadcast'
+                      ]),
             miimon=dict(type='str'),
             mtu=dict(type='int'),
             updelay=dict(type='str'),
@@ -194,18 +211,18 @@ def main():
         required_if=[],
         supports_check_mode=True,
     )
-    module.run_command_environ_update = dict(LANG='C', LC_ALL='C', LC_MESSAGES='C', LC_CTYPE='C')
+    module.run_command_environ_update = dict(
+        LANG='C', LC_ALL='C', LC_MESSAGES='C', LC_CTYPE='C')
 
     networkd = Networkd(module)
 
-    (rc, out, err) = (None, '', '')
     result = {'conn_name': networkd.conn_name, 'state': networkd.state}
     changed = False
 
-
     # check for issues
     if networkd.conn_name is None:
-        networkd.module.fail_json(msg="Please specify a name for the connection")
+        networkd.module.fail_json(
+            msg="Please specify a name for the connection")
     check_milliseconds_field_is_digit(networkd, "miimon")
     check_milliseconds_field_is_digit(networkd, "updelay")
     check_milliseconds_field_is_digit(networkd, "downdelay")
@@ -217,58 +234,44 @@ def main():
             print("Absent")
         elif networkd.state == 'present':
 
-            if networkd.type in ['ethernet','infiniband','bond-slave','vlan']:
+            if networkd.type in [
+                    'ethernet',
+                    'infiniband',
+                    'bond-slave',
+                    'vlan']:
 
                 # Generate Network file
                 network = networkd.generate_network()
                 # Read current Network file if exist and compare if changes
-                network_file = "/etc/systemd/network/" + networkd.conn_name +".network"
+                network_file = "/etc/systemd/network/" +\
+                    networkd.conn_name + ".network"
                 # Check if content is the same
-                changed = not same_list_file(network,network_file)
+                changed = not same_list_file(network, network_file)
                 # Write configuration if changes detected
                 if changed:
                     write_list_to_file(network, network_file)
 
-            if networkd.type in ['bond','vlan']:
+            if networkd.type in ['bond', 'vlan']:
 
                 # Generate Netdev file
                 netdev = networkd.generate_netdev()
                 # Read current Network file if exist and compare if changes
-                netdev_file = "/etc/systemd/network/" + networkd.conn_name +".netdev"
+                netdev_file = "/etc/systemd/network/" +\
+                    networkd.conn_name + ".netdev"
                 # Check if content is the same
-                changed = not same_list_file(netdev,netdev_file)
+                changed = not same_list_file(netdev, netdev_file)
                 # Write configuration if changes detected
                 if changed:
                     write_list_to_file(netdev, netdev_file)
 
-            # if networkd.type in ['vlan']:
-
-            #     # Ensure vlan is registered in main connection (vlandev) network file
-            #     vlandev_file = "/etc/systemd/network/" + networkd.vlandev +".network"
-            #     path = pathlib.Path(vlandev_file)
-            #     os.makedirs(path.parent, mode = 0o755, exist_ok=True)
-            #     f = open(vlandev_file)
-            #     filepath_lines = f.readlines()
-            #     vlan_present = False
-            #     for i in range(0,len(filepath_lines),1):
-            #         if filepath_lines[i] == "VLAN=" + networkd.conn_name:
-            #             vlan_present = True
-            #     if not vlan_present:
-            #         for i in range(0,len(filepath_lines),1):
-            #             if filepath_lines[i] == "[Network]\n":
-            #                 filepath_lines.insert(i+1, "VLAN=" + networkd.conn_name)
-            #                 f.close()
-            #                 f = open(vlandev_file, "w")
-            #                 for j in range(0,len(filepath_lines),1):
-            #                     f.write(filepath_lines[j] + "\n")
-            #                 break
-            #     f.close()
-
-            # Post actions
-            #if changed == 1:
-                #stdout, stderr = subprocess.Popen("networkctl reload", stdout=os.subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()
-                #stdout, stderr = subprocess.Popen("networkctl reconfigure " + networkd.conn_name, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()
-
+# Post actions
+# if changed == 1:
+    # stdout, stderr = subprocess.Popen("networkctl reload",
+    # stdout=os.subprocess.PIPE, stderr=subprocess.STDOUT,
+    # shell=True).communicate()
+    # stdout, stderr = subprocess.Popen("networkctl reconfigure " +
+    # networkd.conn_name, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+    # shell=True).communicate()
 
     except NetworkdModuleError as e:
         module.fail_json(name=networkd.conn_name, msg=str(e))
