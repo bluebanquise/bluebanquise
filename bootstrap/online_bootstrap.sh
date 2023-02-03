ec51f8c31d3710690e22a016a2fa1e37e42d7317
oxedions <oxedions@gmail.com>
2023-02-03 14:42:07 -0500

    Workflow

--- bootstrap/online_bootstrap.sh
+++ bootstrap/online_bootstrap.sh
@@ -75,7 +75,7 @@ fi
 if [ "$VERSION_ID" == "11" ]; then
   sudo apt update
   export DEBIAN_FRONTEND=noninteractive
-  sudo apt install -y python3 python3-pip python3-venv git ssh
+  sudo apt install -y python3 python3-pip python3-venv git ssh curl
 fi
 
 message_output "Creating bluebanquise user, may take a while..."
