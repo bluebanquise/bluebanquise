f6af677ace72002e2df4849632d1b231593f37c8
Thiago Cardozo <thiago.cardozo@eviden.com>
2025-04-20 11:50:55 -0400

    Added expanded nodeset func

--- collections/infrastructure/plugins/filter/nodeset.py
+++ collections/infrastructure/plugins/filter/nodeset.py
@@ -8,6 +8,7 @@ __metaclass__ = type
 
 from ansible.errors import AnsibleError
 from ClusterShell.NodeSet import NodeSet
+from ClusterShell.NodeSet import expand
 from ansible.module_utils.common.text.converters import to_native
 
 
@@ -21,6 +22,15 @@ def nodeset(nodes_list):
 
     return nodeset
 
+def nodeset_expand(nodes_list):
+    '''Convert a list of nodes to expanded ClusterShell's NodeSet'''
+
+    try:
+        nodeset = ",".join(expand(NodeSet(",".join(nodes_list))))
+    except Exception as e:
+        raise AnsibleError('Error joining nodeset, original exception: %s' % to_native(e))
+
+    return nodeset
 
 class FilterModule(object):
     ''' NodeSet Jinja2 filter. '''
